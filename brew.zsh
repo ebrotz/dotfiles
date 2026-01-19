c33026f98153570d031956dad95b60fd15bfa078
Edward Brotz <edward.brotz@gmail.com>
2026-01-19 11:34:19 -0800

    Add ripgrep

--- brew.zsh
+++ brew.zsh
@@ -1,8 +1,13 @@
 #! /bin/zsh
 
+# Linearmouse is used to get rid of natural scrolling when using a traditional
+# mouse.
 brew install --cask linearmouse
 brew install httpie
 brew install --cask font-0xproto-nerd-font
+# A better grep that honors gitignore, skips binaries, and skips hidden files.
+# It will also be used by neovim.
+brew install ripgrep
 
 brew install fd
 brew install neovim
