e270e4a7ad8bb1e4026966e94947402ae7489b86
Edward Brotz <edwardbrotz@microsoft.com>
2026-03-12 13:06:08 -0700

    Add rustup

--- setup.ps1
+++ setup.ps1
@@ -14,6 +14,6 @@ foreach ($b in $targetBuckets) {
 	}
 }
 
-scoop install vim neovim curl wget go lazygit ripgrep fd jq zig cmake luarocks lua unzip gzip stylua curlie rust nvm
+scoop install vim neovim curl wget go lazygit ripgrep fd jq zig cmake luarocks lua unzip gzip stylua curlie rust nvm rustup
 go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
 
