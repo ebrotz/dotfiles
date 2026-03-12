f292a044a363588e7ed6aab68c021bf06b767218
Edward Brotz <edwardbrotz@microsoft.com>
2026-03-12 12:50:13 -0700

    Add curlie, rust, and nvm

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
 scoop bucket add extras
-scoop bucket add extras
-scoop install vim neovim curl wget go lazygit ripgrep fd jq zig cmake luarocks lua unzip gzip stylua
+scoop install vim neovim curl wget go lazygit ripgrep fd jq zig cmake luarocks lua unzip gzip stylua curlie rust nvm
+go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
 
