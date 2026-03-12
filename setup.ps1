cada5a4f11fe3c88d1d4ffecd3ef4f4283c0bc10
Edward Brotz <edwardbrotz@microsoft.com>
2026-03-12 13:01:22 -0700

    Only add buckets that haven't already been added

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,19 @@
-scoop bucket add extras
+$targetBuckets = @('extras', 'nerd-fonts')
+$installedBuckets = $(scoop bucket list)
+
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+	Write-Host "Installing scoop"
+	Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+	Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
+
+foreach ($b in $targetBuckets) {
+	if (!$installedBuckets -contains $b.Name) {
+		Write-Host "Adding scoop bucket $b.Name"
+		scoop bucket add $b.Name
+	}
+}
+
 scoop install vim neovim curl wget go lazygit ripgrep fd jq zig cmake luarocks lua unzip gzip stylua curlie rust nvm
 go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
 
